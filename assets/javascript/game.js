79403b1ba92d65948b3ac96672e254e026a9f9b4
Joey Duchett <joeyduchett@yahoo.com>
2019-06-04 18:40:02 -0700

    added some functionality to keyboard, still need to run statements for logic reasons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,9 +16,37 @@ let winCount = 0;
 let loseCount = 0;
 let guessCounter = 10;
 const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let alphabetStr = 'qwertyuiopasdfghjklzxcvbnm';
-let alphabetArr = alphabetStr.split("");
-console.log(alphabetArr);
+let qwertyStr = 'qwertyuiopasdfghjklzxcvbnm';
+let qwertyArr = qwertyStr.split("");
+console.log(qwertyArr);
 let guessedLetters = [];
+
+function createButtons(qwertyArr){
+    for (let i = 0; i < qwertyArr.length; i++){
+        let keyBtn = $('<button>');
+        let qwerty = qwertyArr[i];
+        qwerty = qwerty.toUpperCase();
+        console.log(qwerty);
+        keyBtn.addClass('keyBtn keyBtn' + qwerty);
+        keyBtn.text(qwerty);
+        $('.keyboard').append(keyBtn);
+        // give button a class, 
+        // text content of array at the index CaretPosition, 
+        // append buttons to keybaoryod div
+    }
+}
+
+// function logArr(qwertyArr) {
+//     console.log(qwertyArr);
+
+// }
+
+// qwertyArr.keypress(function (e) { 
+//     logArr(qwertyArr);
+// });
+
+createButtons(qwertyArr);
+
+
 
 
